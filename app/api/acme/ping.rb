11f1e5828a90202693000239dab460fcac5a334c
Peter Wagenet <peter.wagenet@gmail.com>
2015-07-31 09:57:38 -0700

    Add namespace test

--- app/api/acme/ping.rb
+++ app/api/acme/ping.rb
@@ -4,5 +4,16 @@ module Acme
     get :ping do
       { ping: params[:pong] || 'pong' }
     end
+
+    namespace :super do
+      get :ping do
+        pong = params[:pong] || 'pong'
+        { ping: "#{pong.upcase}!!!" }
+      end
+
+      route :any, '*path' do
+        { path: params[:path] }
+      end
+    end
   end
 end
